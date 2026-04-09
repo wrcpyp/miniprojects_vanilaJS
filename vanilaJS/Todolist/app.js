666d00931ff8044ffcde31077413c88b2302f1f1
tworachet <tangalone1629@gmail.com>
2026-04-09 16:12:04 +0700

    update

--- vanilaJS/Todolist/app.js
+++ vanilaJS/Todolist/app.js
@@ -1,7 +1,8 @@
 const addTaskDom = document.querySelector("#addTask")
-const searchTaskDom = document.querySelector("#searchTask")
 const showTasksDiv = document.querySelector(".showTasks")
 const tasksAmount = document.querySelector(".amount")
+const completedTasksDiv = document.querySelector(".showCompletedTasks")
+
 let amount = 0
 
 document.querySelector(".addTaskBtn").addEventListener("click", () => {
@@ -9,13 +10,16 @@ document.querySelector(".addTaskBtn").addEventListener("click", () => {
     const createEditBtn = document.createElement("button")
     const createDelBtn = document.createElement("button")
     const createCheckbox = document.createElement("input")
+    const textSpan = document.createElement("span")
     createCheckbox.type = "checkbox"
     createEditBtn.classList.add("editBtn")
     createDelBtn.classList.add("delBtn")
+    textSpan.classList.add("textSpan")
     createDelBtn.innerHTML = "Delete"
     createEditBtn.innerHTML = "Edit"
-    createP.innerHTML = addTaskDom.value
+    textSpan.innerHTML = addTaskDom.value
     createP.prepend(createCheckbox)
+    createP.appendChild(textSpan)
     createP.appendChild(createEditBtn)
     createP.appendChild(createDelBtn)
     showTasksDiv.appendChild(createP)
@@ -24,22 +28,59 @@ document.querySelector(".addTaskBtn").addEventListener("click", () => {
 
     createCheckbox.addEventListener("change", () => {
         if (createCheckbox.checked) {
-            createP.classList.add("done")
-        } else {
-            createP.classList.remove("done")
+            completedTasksDiv.appendChild(createP)
         }
     })
 
     createDelBtn.addEventListener("click", () => {
-        createP.innerHTML = ""
+        createP.remove()
         amount--
         tasksAmount.innerHTML = amount
     })
+
+    let isEditing = false
+    const textEdit = document.createElement("input")
+    createEditBtn.addEventListener("click", () => {
+        textSpan.style.display = "none"
+        createP.insertBefore(textEdit, textSpan)
+        textEdit.value = textSpan.innerHTML
+        isEditing = true
+    })
+
 })
 
 document.querySelector(".clearBtn").addEventListener("click", () => {
     showTasksDiv.innerHTML = ""
+    completedTasksDiv.innerHTML = ""
     addTaskDom.value = ""
     amount = 0
     tasksAmount.innerHTML = amount
+
+})
+
+const completedTasksFilter = document.querySelector("#completedTasks")
+completedTasksFilter.type = "checkbox"
+document.querySelector("#completedTasks").addEventListener("change", () => {
+    const h4 = document.querySelector(".showTasksTitle")
+    if (completedTasksFilter.checked) {
+        showTasksDiv.style.display = "none"
+        h4.style.display = "none"
+    } else {
+        showTasksDiv.style.display = "block"
+        h4.style.display = "block"
+    }
+
+})
+
+const activeTasksFilter = document.querySelector("#activeTasks")
+activeTasksFilter.type = "checkbox"
+document.querySelector("#activeTasks").addEventListener("change", () => {
+    const h4 = document.querySelector(".completedTasksTitle")
+    if (activeTasksFilter.checked) {
+        completedTasksDiv.style.display = "none"
+        h4.style.display = "none"
+    } else {
+        completedTasksDiv.style.display = "block"
+        h4.style.display = "block"
+    }
 })
