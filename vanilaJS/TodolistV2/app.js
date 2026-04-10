ce677280c437498bfc83534341b44ed91e6642ca
twrc <tangalone1629@gmail.com>
2026-04-10 10:29:07 +0700

    update

--- vanilaJS/TodolistV2/app.js
+++ vanilaJS/TodolistV2/app.js
@@ -1,5 +1,6 @@
 let tasks = [];
 const input = document.querySelector("#addTask");
+
 function addTask(text) {
   if (text.trim() === "") {
     return;
@@ -19,13 +20,23 @@ function deleteTask(id) {
 }
 
 function toggleTask(id) {
-    tasks = tasks.map(task => {
-        if (task.id === id) {
-            return {...task, completed: !task.completed}
-        }
-        return task
-    })
-    render()
+  tasks = tasks.map((task) => {
+    if (task.id === id) {
+      return { ...task, completed: !task.completed };
+    }
+    return task;
+  });
+  render();
+}
+
+function editTask(id, newText) {
+  tasks = tasks.map((task) => {
+    if (task.id === id) {
+      return {...task, text: newText}
+    }
+    return task
+  })
+  render()
 }
 
 const showTaskDiv = document.querySelector(".showTaskDiv");
@@ -36,24 +47,37 @@ function render() {
   tasks.forEach((task) => {
     const p = document.createElement("p");
     const delBtn = document.createElement("button");
-    const checkbox = document.createElement("input")
-    checkbox.type = "checkbox"
-    p.textContent = task.text;
+    const editBtn = document.createElement("button");
+    const checkbox = document.createElement("input");
+    const span = document.createElement("span")
+    checkbox.type = "checkbox";
+    span.textContent = task.text;
+    editBtn.textContent = "Edit";
     delBtn.textContent = "Delete";
     showTaskDiv.appendChild(p);
+    p.appendChild(span)
+    p.appendChild(editBtn);
     p.appendChild(delBtn);
-    p.prepend(checkbox)
+    p.prepend(checkbox);
+
+    editBtn.addEventListener("click", () => {
+      const newText = prompt("Edit task : ", task.text)
+      if (newText === null || newText.trim() === "") {
+        return
+      }
+      editTask(task.id, newText)
+    })
 
     delBtn.addEventListener("click", () => {
       deleteTask(task.id);
     });
 
     checkbox.addEventListener("change", () => {
-        toggleTask(task.id)
-    })
+      toggleTask(task.id);
+    });
 
     if (task.completed) {
-        p.style.textDecoration = "line-through"
+      p.style.textDecoration = "line-through";
     }
   });
 }
